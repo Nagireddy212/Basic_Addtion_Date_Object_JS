856e570d451525247b885de5f2659904770bbd0f
Ngireddy <somulanagireddy@gmail.com>
2021-04-20 20:38:57 +0530

    [Nagireddy]: added Basic javaScript programm

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+{
+const IS_ABSCENT = 0
+let empCheck = Math.floor(Math.random() *10) %2;
+if (empCheck == IS_ABSCENT) {
+    console.log("UC1 - Employee is ABSCENT")
+}else {
+    console.log("UC1 - Employee is PRESENT")
+}
+}
