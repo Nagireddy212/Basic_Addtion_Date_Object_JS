33eb6512ca7d6f2509f1f99bd3aa71c1e6cd0611
Nagireddy212 <79092203+Nagireddy212@users.noreply.github.com>
2021-04-21 18:26:06 +0530

    Delete Javascript.js

--- Javascript.js
+++
@@ -1,9 +0,0 @@
-{
-const IS_ABSCENT = 0
-let empCheck = Math.floor(Math.random() *10) %2;
-if (empCheck == IS_ABSCENT) {
-    console.log("UC1 - Employee is ABSCENT")
-}else {
-    console.log("UC1 - Employee is PRESENT")
-}
-}
